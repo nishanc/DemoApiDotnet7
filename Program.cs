b67ea3a9f36b17348451adfc60fae6c93c9a682b
Nishan Wickramarathna <inbox.nishan@gmail.com>
2023-01-05 00:12:26 +0530

    Configure connection string in Program.cs class

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using DemoApiDotnet7.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
