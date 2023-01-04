2351c27c92bd00901bab875493eab6c7d07200bd
Nishan Wickramarathna <inbox.nishan@gmail.com>
2023-01-05 00:38:51 +0530

    After "dotnet ef migrations add AddedWorkStationAndRelationship"

---
+++ Migrations/20230104190726_AddedWorkStationAndRelationship.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoApiDotnet7.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedWorkStationAndRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkStationId",
+                table: "Assets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "WorkStations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkStations", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assets_WorkStationId",
+                table: "Assets",
+                column: "WorkStationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assets_WorkStations_WorkStationId",
+                table: "Assets",
+                column: "WorkStationId",
+                principalTable: "WorkStations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assets_WorkStations_WorkStationId",
+                table: "Assets");
+
+            migrationBuilder.DropTable(
+                name: "WorkStations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Assets_WorkStationId",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "WorkStationId",
+                table: "Assets");
+        }
+    }
+}
