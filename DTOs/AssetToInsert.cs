3c1ae2fdb51d9600aa61b461b425a6fc601ddbe2
Nishan Wickramarathna <inbox.nishan@gmail.com>
2023-01-05 20:49:03 +0530

    Add Asset DTOs

---
+++ DTOs/AssetToInsert.cs
@@ -0,0 +1,9 @@
+namespace DemoApiDotnet7.DTOs
+{
+    public class AssetToInsert
+    {
+        public string ItemCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DTOs/AssetToReturn.cs
@@ -0,0 +1,10 @@
+namespace DemoApiDotnet7.DTOs
+{
+    public class AssetToReturn
+    {
+        public int Id { get; set; }
+        public string ItemCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
