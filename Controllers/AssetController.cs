47df3bb5d9b833f160b2e1cd9c9b249afa861f07
Nishan Wickramarathna <inbox.nishan@gmail.com>
2023-01-05 20:53:42 +0530

    Get Asset by Id endpoint

--- Controllers/AssetController.cs
+++ Controllers/AssetController.cs
@@ -1,5 +1,7 @@
 using DemoApiDotnet7.Data;
+using DemoApiDotnet7.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoApiDotnet7.Controllers
 {
@@ -11,6 +13,26 @@ namespace DemoApiDotnet7.Controllers
         public AssetController(DataContext context)
         {
             _context = context;
+        }
+
+        // GET api/asset/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsset(int id)
+        {
+            var value = await _context.Assets.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (value is null){
+                return NotFound();
+            }
+
+            var assetToReturn = new AssetToReturn{
+                Id = value.Id,
+                ItemCode = value.ItemCode,
+                Name = value.Name,
+                Description = value.Description
+            };
+            
+            return Ok(value);
         }
     }
 }
